5f382450b143dfdfcb7f64e2ba00be4c06f20135
Salah Kaifas <salah.kaifas@gmail.com>
2024-02-19 13:49:12 +0100

    test

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const AVATAR_API = 'https://ui-avatars.com/api';
-export const API = 'https://hse-space.onrender.com';
+export const API = 'https://hse-space.onrender.com/api';
 export const AUTH_TOKEN = 'authToken';
 export const BEARER = 'Bearer';
